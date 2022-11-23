2f42be2e8b2689c5c9f30b693675f8049bd5dc41
Kristopher Gray <kristopher.gray99@gmail.com>
2022-11-23 15:22:24 +0100

    map students -> shuffle -> slice. Add to DOM buttons

--- script.js
+++ script.js
@@ -1,49 +1,77 @@
 /*
 * 
+* Steg 1: Hämta ut ett namn från arrayn till första knappen
 *
+* Steg 2: Hämta ut ett random namn från arrayn till knappen
 *
+* Steg 3: Ta det random namnet från arrayn och lägg randomly på en av de 4 knapparna
 *
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
+* Shuffle copy of array and then chose the first 4 slots for the 4 buttons
 *
 */
 
 const guessFormEl = document.querySelector('#guessForm');
+const btnPerson1El = document.querySelector('#btnPerson1');
+const btnPerson2El = document.querySelector('#btnPerson2');
+const btnPerson3El = document.querySelector('#btnPerson3');
+const btnPerson4El = document.querySelector('#btnPerson4');
 const imageContainer = document.querySelector('#imageContainer');
 
 let students = [
     {
         name: 'Kristopher',
-        image: '',
+        image: 'Kristophers bild',
     },
     {
         name: 'Jonas',
-        image: '',
+        image: 'Jonas bild',
     },
     {
         name: 'Harald',
-        image: '',
+        image: 'Haralds bild',
     },
     {
         name: 'Joakim',
-        image: '',
+        image: 'Joakims bild',
+    },
+    {
+        name: 'Patrik',
+        image: 'Patriks bild',
+    },
+    {
+        name: 'Simon',
+        image: 'Simons bild',
     },
 ];
 
-guessFormEl.btnPerson1.addEventListener('click', e => {
-    e.preventDefault();
+// FisherYates random number algorithm
+const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
 
-    console.log('Person 1 submitted');
-});
+// Make a copy of students
+const newStudents = students.map(student => student);
+// Shuffle newStudents (destructive function)
+shuffleArray(newStudents);
+// Save the first 4 students AFTER shuffling
+const shuffledStudents4 = newStudents.slice(0, 4);
 
+
+
+btnPerson1El.textContent = `${shuffledStudents4[0].name}`;
+btnPerson2El.textContent = `${shuffledStudents4[1].name}`;
+btnPerson3El.textContent = `${shuffledStudents4[2].name}`;
+btnPerson4El.textContent = `${shuffledStudents4[3].name}`;
+
+// let studentsRandomIndex = getRandomNumber(students.length) - 1;
+// const studentsRandomName = students[getRandomNumber(students.length) - 1].name;
+
+// btnPerson1El.textContent = `${students[getRandomNumber(students.length) - 1].name}`;
+// btnPerson2El.textContent = `${students[getRandomNumber(students.length) - 1].name}`;
+// btnPerson3El.textContent = `${students[getRandomNumber(students.length) - 1].name}`;
+// btnPerson4El.textContent = `${students[getRandomNumber(students.length) - 1].name}`;
